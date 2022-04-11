eeca5ec05ede4690f2bd09b450020bb0100dbd18
ergulyunus <ergulyunus@gmail.com>
2022-04-12 01:46:57 +0300

    LongestCommonPrefix solution added.

---
+++ LeetCode/ProblemSolutions/EasyProblems/LongestCommonPrefix_14.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.ProblemSolutions.EasyProblems
+{
+    public static class LongestCommonPrefix_14
+    {
+        public static string Solution(string[] strs)
+        {
+            int length = strs.Length;
+            if (length == 0) return "";
+            if (length == 1) return strs[0];
+
+            int count = strs[0].Length;
+            string prefix = strs[0];
+
+            for (int i = 1; i < length; i++)
+            {
+                int strsLength = strs[i].Length;
+                for (int j = 0; j < count; j++)
+                {
+                    if (j == strsLength || strs[i][j] != prefix[j])
+                    {
+                        prefix = prefix.Substring(0, j).ToLowerInvariant();
+                        count = prefix.Length;
+                        if (count == 0) return "";
+                        break;
+                    }
+                }
+            }
+
+            return prefix;
+        }
+    }
+}
---
+++ LeetCodeUnitTests/EasyProblemsTest/LongestCommonPrefixTest.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.ProblemSolutions.EasyProblems;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeUnitTests.EasyProblemsTest
+{
+    public class LongestCommonPrefixTest
+    {
+        [Fact]
+        public void IsPalindrome_With_Correct_Request_Parameter_Then_OK_Test()
+        {
+            string[] testparams = new string[3] { "flight", "flow", "flower" };
+
+            var result = LongestCommonPrefix_14.Solution(testparams);
+            Assert.Equal("fl",result);
+        }
+    }
+}
